dda1fd0d0ceb400df6418bbf55affe1beaf50c10
David Hawkins <uxhawk@gmail.com>
2020-04-16 18:55:43 -0400

    added get routes for index and notes.html

--- Develop/server.js
+++ Develop/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+var path = require("path");
 
 const app = express();
 app.use(express.urlencoded({
@@ -13,4 +14,14 @@ const port = process.env.port || 8080;
 
 app.listen(port, function() {
     console.log("App listening on PORT: " + port);
+});
+
+// Basic route that sends the user first to the Page
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
