d9d4d8914e6e694abd930b7d830097f55cd16eae
David Hawkins <uxhawk@gmail.com>
2020-04-17 14:32:37 -0400

    added function set unique ids for each note in db.json

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,6 +1,6 @@
 const notes = require("../db/db.json");
-const path = require("path");
-const fs = require("fs");
+// const path = require("path");
+// const fs = require("fs");
 
 module.exports = function(app) {
 
@@ -8,17 +8,18 @@ module.exports = function(app) {
         return (res.json(notes));
     });
 
-    // app.post("/api/tables", function(req, res) {
-    //     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    //     // It will do this by sending out the value "true" have a table
-    //     // req.body is available since we're using the body parsing middleware
-    //     if (tableData.length < 5) {
-    //       tableData.push(req.body);
-    //       res.json(true);
-    //     }
-    //     else {
-    //       waitListData.push(req.body);
-    //       res.json(false);
-    //     }
-    //   });
+
+
+    app.post("/api/notes", function(req, res) {
+        notes.push(req.body);
+        setNoteID();
+        console.log(`added to notes`);
+    });
+
+}
+
+function setNoteID() {
+    notes.forEach((el, index) => {
+        el.id = index
+    });
 }
