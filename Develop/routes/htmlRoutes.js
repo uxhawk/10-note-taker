ba8ef4a0cf15549c7f2f02661950b0a5b1dfb752
David Hawkins <uxhawk@gmail.com>
2020-04-17 12:06:39 -0400

    fixed file path for serving html files in htmlRoutes.js

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -3,11 +3,11 @@ const path = require("path");
 module.exports = function(app) {
     // Basic route that sends the user first to the Page
     app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname, "./public/index.html"));
+        res.sendFile(path.join(__dirname, "../public/index.html"));
     });
 
     app.get("/notes", function(req, res) {
-        res.sendFile(path.join(__dirname, "./public/notes.html"));
+        res.sendFile(path.join(__dirname, "../public/notes.html"));
     });
 
     //404 page needs to go here
--- Develop/server.js
+++ Develop/server.js
@@ -2,14 +2,16 @@ const express = require("express");
 const app = express();
 const port = process.env.port || 8080;
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+
 
 app.use(express.urlencoded({
     extended: true
 }));
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(port, function() {
     console.log("App listening on PORT: " + port);
