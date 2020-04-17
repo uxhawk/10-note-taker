0548bac4e9390144ebfdb037f3977faaee385012
David Hawkins <uxhawk@gmail.com>
2020-04-17 17:54:37 -0400

    unique ids for notes start at , click in note list populates main part of page

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,6 +1,15 @@
 const notes = require("../db/db.json");
 // const path = require("path");
-// const fs = require("fs");
+const fs = require("fs");
+const rawData = fs.readFileSync("db/db.json");
+var notesArr = JSON.parse(rawData);
+
+// const util = require("util");
+// const writeFileAsync = util.promisify(fs.writeFile);
+
+function isEmpty(obj) {
+    return Object.keys(obj).length === 0;
+}
 
 module.exports = function(app) {
 
@@ -8,18 +17,31 @@ module.exports = function(app) {
         return (res.json(notes));
     });
 
-
-
-    app.post("/api/notes", function(req, res) {
-        notes.push(req.body);
-        setNoteID();
-        console.log(`added to notes`);
+    app.get("/api/notes/:id", function(req, res) {
+        var id = req.params.id;
+        return res.json(notes[id]);
     });
 
+    app.post("/api/notes", async function(req, res) {
+        if (isEmpty(notes)) {
+            notesArr = [];
+        }
+        notesArr.push(req.body);
+        setNoteID();
+
+        var notesData = JSON.stringify(notesArr, null, 4);
+
+        fs.writeFile("db/db.json", notesData, function(err) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log("Note added to file");
+        });
+    });
 }
 
 function setNoteID() {
-    notes.forEach((el, index) => {
-        el.id = index
+    notesArr.forEach((el, index) => {
+        el.id = index + 1
     });
 }
