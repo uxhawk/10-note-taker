4df92f5cbf588211d986ba7fff1a1aeb29612c59
David Hawkins <uxhawk@gmail.com>
2020-04-19 14:42:24 -0400

    delete route works, was missing a / before the path

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,5 @@
 var notes = require("../db/db.json");
 const fs = require("fs");
-const rawData = fs.readFileSync("db/db.json");
-var notesArr = JSON.parse(rawData);
-
 const util = require("util");
 const writeFileAsync = util.promisify(fs.writeFile);
 
@@ -12,37 +9,38 @@ function isEmpty(obj) {
 }
 
 module.exports = function(app) {
-
     app.get("/api/notes", function(req, res) {
         return (res.json(notes));
     });
 
     app.get("/api/notes/:id", function(req, res) {
         let id = req.params.id;
+
         return res.json(notes[id - 1]);
     });
 
     app.post("/api/notes", async function(req, res) {
-        //if you don't have any saved notes, reset the notes array to be empty
         if (isEmpty(notes)) {
             notes = [];
         }
 
         notes.push(req.body);
-        //run function to set unique ids to each element in the array
         setNoteID();
 
-        //stringify notes array, then write it to the db.json file
         let notesData = JSON.stringify(notes, null, 4);
         await setSavedNotes(notesData);
 
         res.json(req.body);
     });
 
-    app.delete("api/notes/:id", async function(req, res) {
-        let id = req.params.id;
+    app.delete("/api/notes/:id", async function(req, res) {
+        var id = req.params.id;
 
-        deleteNote(id);
+        notes.forEach((el, index) => {
+            if (el.id == id) {
+                notes.splice(index, 1);
+            }
+        });
         setNoteID();
 
         let notesData = JSON.stringify(notes, null, 4);
@@ -63,13 +61,4 @@ function setSavedNotes(content) {
     } catch (err) {
         console.log(err);
     }
-}
-
-function deleteNote(noteID) {
-    // console.log(notes);
-    notes.forEach((el, index) => {
-        if (el.id === noteID) {
-            notes.splice(index, 1);
-        }
-    });
 }
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -11,8 +11,8 @@ module.exports = function(app) {
     });
 
     // If no matching route is found default to home
-    // app.get("/*", function(req, res) {
-    //     res.sendFile(path.join(__dirname, "../public/index.html"));
-    // });
+    app.get("/*", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
 
 }
